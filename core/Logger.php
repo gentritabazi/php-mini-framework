cfdd1a37f129cbf37c03daad18e6b89d2330f232
Gentrit Abazi <gentritabazi01@gmail.com>
2020-08-23 10:24:30 +0200

    Added Logger.

---
+++ core/Logger.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Core;
+
+class Logger
+{
+    const EMERGENCY = 'emergency';
+    const ALERT = 'alert';
+    const CRITICAL = 'critical';
+    const ERROR = 'error';
+    const WARNING = 'warning';
+    const NOTICE = 'notice';
+    const INFO = 'info';
+    const DEBUG = 'debug';
+
+    private static $instance;
+    
+    private static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new Logger();
+        }
+
+        return self::$instance;
+    }
+
+    private function writeToFile($message)
+    {
+        file_put_contents(BASE_DIR. '/logs/log-'. date('Y-m-d'). '.txt', "$message\n", FILE_APPEND);
+    }
+
+    public static function log($message, $level = Logger::INFO)
+    {
+        $date = date('Y-m-d H:i:s');
+        $severity = "[$level]";
+        $message = "$date $severity : $message";
+        self::getInstance()->writeToFile($message);
+    }
+}
