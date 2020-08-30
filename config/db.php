ec909d2daf2fd45cc3e27ca9460e931dc409cf68
Gentrit Abazi <gentritabazi01@gmail.com>
2020-08-30 09:43:41 +0200

    Changed db.

--- config/db.php
+++ config/db.php
@@ -2,6 +2,6 @@
 
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '8889');
-define('DB_NAME', 'mybox');
+define('DB_NAME', 'larapi');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
