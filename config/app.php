0a36e8cf75104eef7d87b89d804be5651e1b2278
Gentrit Abazi <gentritabazi@Gentrit-Abazi.local>
2020-09-05 18:35:39 +0200

    Rechanged.

--- config/app.php
+++ config/app.php
@@ -4,4 +4,4 @@ define('APP_START', microtime(true));
 
 define('SHOW_ERRORS', true);
 
-define('APP_SUB_DIRECTORY', '/GitHub/pure-php/public');
+define('APP_SUB_DIRECTORY', '/GitHub/pure-clean-php/public');
